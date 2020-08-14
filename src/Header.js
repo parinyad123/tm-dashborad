2dc11066f4ee24e1e7db769ddaaf4bacfbea2dca
parinyad123 <sci.equ@gmail.com>
2020-08-14 23:38:09 +0700

    first start

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+// คำสั่ง rfce ใช้สร้าง function Header
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>I am header</h1>
+            
+        </div>
+    )
+}
+
+export default Header;
